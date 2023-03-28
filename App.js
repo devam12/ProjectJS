1107d5d150ed386f550397c4a080605c5e691cec
Devam <devampanchasara12@gmail.com>
2023-03-28 18:54:19 +0530

    video 12 completed

--- App.js
+++ App.js
@@ -3,31 +3,33 @@ import {
   Text,
   View,
   Button,
+  TextInput,
 } from 'react-native';
 import CompanyData from './components/CompanyData'
 
 const App =  () =>  {
-  const [data,setData] = useState(10) 
-  const fun = (val)=>{
-    setData(20)
-    console.warn("Button Called " + val);
-  }
+  const [data,setData] = useState("") 
+  // const fun = (val)=>{
+  //   setData(20)
+  //   console.warn("Button Called " + val);
+  // }
   return (
     <View>
-        <CompanyData />
-        <SecondCompuonent data={data} />
-        <Text style={{color : "red" , fontSize: 25}}>{props.data}</Text>
-        <Button title="press here" onPress={()=>{fun("Devam")}}></Button>
+        {/* <CompanyData />
+        <SecondCompuonent data={data} /> */}
+        <TextInput style={{borderColor:'red' }} onChangeText={(value)=>{setData(value)}} value={data} placeholder="Enter Text"></TextInput>
+        <Text style={{color : "red" , fontSize: 25}}>{data}</Text>
+        <Button title="press here" onPress={()=>{setData('')}}></Button>
     </View>
   );
 };
 
-const SecondCompuonent =  (props) =>  {
-  return (
-    <View>
-        <Text style={{color : "red" , fontSize: 25}}>Second Compuonent data : {props.data}</Text>
-    </View>
-  );
-};
+// const SecondCompuonent =  (props) =>  {
+//   return (
+//     <View>
+//         <Text style={{color : "red" , fontSize: 25}}>Second Compuonent data : {props.data}</Text>
+//     </View>
+//   );
+// };
 
 export default App;
