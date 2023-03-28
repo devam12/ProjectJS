3c592bd87155a75f0e54028bf9e17243dbfa9b6a
Devam <devampanchasara12@gmail.com>
2023-03-28 20:51:14 +0530

    video 13 completed

--- App.js
+++ App.js
@@ -4,32 +4,50 @@ import {
   View,
   Button,
   TextInput,
+  StyleSheet
 } from 'react-native';
-import CompanyData from './components/CompanyData'
 
-const App =  () =>  {
-  const [data,setData] = useState("") 
-  // const fun = (val)=>{
-  //   setData(20)
-  //   console.warn("Button Called " + val);
-  // }
+const App = () => {
+  const [usename, setUsename] = useState("")
+  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [display, setDisplay] = useState(false);
+
+  const displayData = () => {
+    return (
+      <>
+
+      </>
+    )
+  }
   return (
-    <View>
-        {/* <CompanyData />
-        <SecondCompuonent data={data} /> */}
-        <TextInput style={{borderColor:'red' }} onChangeText={(value)=>{setData(value)}} value={data} placeholder="Enter Text"></TextInput>
-        <Text style={{color : "red" , fontSize: 25}}>{data}</Text>
-        <Button title="press here" onPress={()=>{setData('')}}></Button>
-    </View>
+    <>
+      <View>
+        <TextInput style={styles.text} onChangeText={(value) => { setUsename(value) }} value={usename} placeholder="Enter username"></TextInput>
+        <TextInput style={styles.text} onChangeText={(value) => { setPassword(value) }} value={password} secureTextEntry={true} placeholder="Enter password"></TextInput>
+        <TextInput style={styles.text} onChangeText={(value) => { setEmail(value) }} value={email} placeholder="Enter email"></TextInput>
+        <Button title="Clear Text" onPress={() => { setUsename(''); setPassword(''); setEmail(''); setDisplay(false); }}></Button>
+        <Button title="Display Data" onPress={() => { setDisplay(true) }}></Button>
+      </View>
+      {
+        display ?
+          <View>
+            <Text style={{ color: "red", fontSize: 20 }}>{usename}</Text>
+            <Text style={{ color: "red", fontSize: 20 }}>{password}</Text>
+            <Text style={{ color: "red", fontSize: 20 }}>{email}</Text> 
+          </View> : null
+      }
+    </>
   );
 };
 
-// const SecondCompuonent =  (props) =>  {
-//   return (
-//     <View>
-//         <Text style={{color : "red" , fontSize: 25}}>Second Compuonent data : {props.data}</Text>
-//     </View>
-//   );
-// };
+const styles = StyleSheet.create({
+  text: {
+    borderColor: 'red',
+    borderWidth: 2,
+    margin: 5,
+    padding: 2,
+  }
+})
 
 export default App;
