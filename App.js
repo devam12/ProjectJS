fae0f9d94d3d06f53089453a0c973626e48c8948
Devam <devampanchasara12@gmail.com>
2023-03-29 16:56:09 +0530

    Responsive flext color done

--- App.js
+++ App.js
@@ -1,43 +1,52 @@
-import React, { useEffect, useInsertionEffect, useLayoutEffect, useState } from 'react';
-import { Text, View, FlatList, Button, TextInput } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
 const App = ()=>{
-  const[count,setCount] = useState(0);
-  const[data,setData] = useState(100);
-  const[show,setShow] = useState(true);
-
     return (
-      <View>
-        <Text>Main Class count : {count}</Text>
-        <Text>Main Class data : {data}</Text>
-        <Button title='Click me' onPress={()=>{setShow(!show)}} />
-        <TextInput placeholder='Enter name : ' onChangeText={(text)=>{setCount(text.length)}}></TextInput>
-        {
-          show ? <Second data={data} count={count}/> : null
-        }
+      <View style={styles.main}>
+        <View style={styles.box1}>
+          <View style={styles.innerbox1}></View>
+          <View style={styles.innerbox2}></View>
+          <View style={styles.innerbox3}></View>
+        </View>
+        <View style={styles.box2}></View>
+        <View style={styles.box3}></View>
       </View>
     )
 }
 
-const Second = (props)=>{
+const styles = StyleSheet.create({
+  main:{
+    flex:1,
+    backgroundColor:'black'
+  },
+  box1:{
+    flex:2,
+    backgroundColor:"red",
+    flexDirection : 'row'
+  },
+  box2:{
+    flex:1,
+    backgroundColor:"green"
+  },
+  box3:{
+    flex:1,
+    backgroundColor:"yellow"
+  },
+  innerbox1:{
+    flex:1,
+    backgroundColor : 'red'
+  },
+  innerbox2:{
+    flex:1,
+    backgroundColor : 'blue'
+  },
+  innerbox3:{
+    flex:1,
+    backgroundColor : 'darkgray'
+  }
+})
 
-  let count = props.count;
-  let data = props.data;
 
-  useEffect(()=>{
-    return ()=>{console.warn("child",count);}
-  },[count])
-
-  useEffect(()=>{
-    return ()=>{console.warn("child",data);}
-  },[data])
-
-    return (
-      <View>
-        <Text>Child Class count : {props.count}</Text>
-        <Text>Child Class data : {props.data}</Text>
-      </View>
-    )
-}
 
 export default App;
